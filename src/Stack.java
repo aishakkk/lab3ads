d967bebeb76571d2b826819a0caea200bc36c95a
zbekxz <forestnatplay@yandex.ru>
2023-04-24 17:23:57 +0600

    adding some documentation to interface Stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,7 +1,7 @@
 public interface Stack<T> {
-    void push(T item);
-    T pop();
-    T peek();
-    boolean isEmpty();
-    int size();
+    void push(T item); // adds an element to the top of the stack
+    T pop(); //  removes and returns the top element of the stack
+    T peek(); // returns the top element of the stack without removing it
+    boolean isEmpty(); // returns true if the stack is empty or false if it is not
+    int size(); // returns the number of elements in the stack
 }
