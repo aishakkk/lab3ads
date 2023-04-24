e53ee0c17154c84715be4ae0c821d8ea80f41849
zbekxz <forestnatplay@yandex.ru>
2023-04-24 17:24:45 +0600

    adding some documentation to interface Queue

--- src/Queue.java
+++ src/Queue.java
@@ -1,7 +1,7 @@
 public interface Queue<T> {
-    void enqueue(T item);
-    T dequeue();
-    T peek();
-    boolean isEmpty();
-    int size();
+    void enqueue(T item); // adds an element to the back of the queue
+    T dequeue(); // removes and returns the front element of the queue
+    T peek(); // returns the front element of the queue without removing it
+    boolean isEmpty(); // returns true if the queue is empty or false if it is not
+    int size(); // returns the number of elements in the queue
 }
