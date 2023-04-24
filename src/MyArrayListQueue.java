f3c8476995ca84bac30a4969a25be888d055c3e0
zbekxz <forestnatplay@yandex.ru>
2023-04-24 16:49:01 +0600

    adding constructor MyArrayListQueue and implementing necessary methods

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,4 +1,31 @@
 public class MyArrayListQueue<T> implements Queue<T> {
     private MyArrayList<T> list;
+    public MyArrayListQueue() {
+        this.list = new MyArrayList<>();
+    }
 
+    @Override
+    public void enqueue(T item) {
+
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,7 @@
 public interface Queue<T> {
+    void enqueue(T item);
+    T dequeue();
+    T peek();
+    boolean isEmpty();
+    int size();
 }
